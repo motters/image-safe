8c68c35b81a1a6997546e53ac5c4e34c4d2a4517
Sam Mottley <sammottley@gmail.com>
2016-01-06 20:24:19 +0000

    started to add the ability for the package to support multiple file uploads

--- src/ImageSafe.php
+++ src/ImageSafe.php
@@ -57,16 +57,47 @@ class ImageSafe {
 	}
 
 	/**
+	 * Support multiple file upload
+	 * @param int $file
+	 * @return array
+	 */
+	public function selectFile($file)
+	{
+		return [
+			'name' => $this->file['name'][$file],
+			'type' => $this->file['type'][$file],
+			'tmp_name' => $this->file['tmp_name'][$file],
+			'error' => $this->file['error'][$file],
+			'size' => $this->file['size'][$file]
+		];
+	}
+
+	/**
+	 * Run though verifications
+	 */
+	public function runVerifications($verifier, $file)
+	{
+		if(!empty($this->rules[str_replace('check', 'image', $verifier)]) or in_array(str_replace('check', 'image', $verifier), $this->rules) ){
+			$class = "Motters\\ImageSafe\\Verifiers\\".$verifier;
+			$verify = new $class($file, $this->rules);
+			return $verify->valid();
+		}
+		return true;
+	}
+
+	/**
 	 * Runs the associated verifier for the verification method
 	 * @pram $library name of the library
 	 * @return bool
 	 */
 	public function verifyForVerificationMethod( $verifier )
-	{	
-		if(!empty($this->rules[str_replace('check', 'image', $verifier)]) or in_array(str_replace('check', 'image', $verifier), $this->rules) ){
-			$class = "Motters\\ImageSafe\\Verifiers\\".$verifier;
-			$verify = new $class($this->file, $this->rules);
-			return $verify->valid();
+	{
+		if(is_array($this->file['name'])){
+			foreach($this->file['name'] as $no => $name){
+				$result = $this->runVerifications($verifier, $this->selectFile($no));
+				if(!$result)
+					return $result;
+			}
 		}
 		return true;
 	}
